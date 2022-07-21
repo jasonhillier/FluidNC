72949bf482bf59d1668cb734ad0ca8cc514cfbbc
jasonhillier <jason@hillier.us>
2022-07-21 16:47:28 -0600

    bldc_servo working

--- FluidNC/src/Motors/BldcServo.cpp
+++ FluidNC/src/Motors/BldcServo.cpp
@@ -15,6 +15,7 @@ namespace MotorDrivers {
         RcServo::init();
 
         _dir_pin.setAttr(Pin::Attr::Output);
+        _stop_pin.setAttr(Pin::Attr::Output);
     }
 
     void BldcServo::set_location() {
@@ -29,8 +30,11 @@ namespace MotorDrivers {
 
         float mpos = steps_to_mpos(get_axis_motor_steps(_axis_index), _axis_index);  // get the axis machine position in mm
         servo_pos  = mpos;                                         // determine the current work position
+
+        servo_pos ? _stop_pin.on() : _stop_pin.off();
         
-        if (servo_pos > 0)
+        //log_info("servo_pos: " << servo_pos);
+        if (servo_pos >= 0)
         {
             _dir_pin.off();
         }
@@ -41,9 +45,9 @@ namespace MotorDrivers {
 
         // determine the pulse length
         servo_pulse_len = static_cast<uint32_t>(mapConstrain(
-            servo_pos, limitsMinPosition(_axis_index), limitsMaxPosition(_axis_index), (float)_min_pulse_cnt, (float)_max_pulse_cnt));
+            fabs(servo_pos), 0.0f, fabs(limitsMinPosition(_axis_index)), (float)_min_duty, (float)_max_duty));
 
-        //log_info("su " << servo_pulse_len);
+        //log_info("pwm: " << servo_pulse_len << " [" << _min_duty << "|" << _max_duty);
 
         _write_pwm(servo_pulse_len);
     }
@@ -51,6 +55,17 @@ namespace MotorDrivers {
     void BldcServo::validate() const {
         RcServo::validate();
         
-        Assert(_dir_pin.defined(), "Step pin must be configured.");
+        Assert(_dir_pin.defined(), "Dir pin must be configured.");
+        //stop_pin optional
+    }
+
+    float BldcServo::fabs(const float p)
+    {
+        return p > 0 ? p : p*-1.0f;
+    }
+
+    // Configuration registration
+    namespace {
+        MotorFactory::InstanceBuilder<BldcServo> registration("bldc_servo");
     }
 }
--- FluidNC/src/Motors/BldcServo.h
+++ FluidNC/src/Motors/BldcServo.h
@@ -7,13 +7,20 @@
 
 namespace MotorDrivers {
     class BldcServo : public RcServo {
+    private:
+        float fabs(const float p);
+
     protected:
         Pin _dir_pin;
+        Pin _stop_pin;
+        uint32_t _min_duty = 0;
+        uint32_t _max_duty = SERVO_PWM_MAX_DUTY;
 
         void set_location() override;
-        void validate() const override;
+        void validate() const override;        
 
     public:
+        BldcServo() = default;
 
         void init() override;
 
@@ -21,9 +28,14 @@ namespace MotorDrivers {
             RcServo::group(handler);
 
             handler.item("direction_pin", _dir_pin);
+            handler.item("stop_pin", _stop_pin);
+            handler.item("min_duty", _min_duty);
+            handler.item("max_duty", _max_duty);
         }
 
         // Name of the configurable. Must match the name registered in the cpp file.
         const char* name() const override { return "bldc_servo"; }
+
+        ~BldcServo() = default;
     };
 }
